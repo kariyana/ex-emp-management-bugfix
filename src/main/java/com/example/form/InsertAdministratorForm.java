1838d79fbb566509ed11a43efc8c1add415bff3c
kariyana <naoki.kariya@rakus-partners.co.jp>
2024-02-09 16:51:12 +0900

    validationを設定し、エラー文を表示できるようにした

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +12,15 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "名前を入力してください")
 	private String name;
 	/** メールアドレス */
+	@Email
+	@NotBlank(message = "メールアドレスを入力してください")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message = "パスワードを入力してください")
+	@Pattern(regexp = "^[a-zA-Z0-9.?/-]{8,24}$" ,message = "８文字以上２４文字以内で入力してください。")
 	private String password;
 
 	public String getName() {
